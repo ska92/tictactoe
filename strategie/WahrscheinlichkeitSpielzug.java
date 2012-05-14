592142c2a402364b214da3f9de9e6bbbe58f831b
Christian <christian.wernicke1@web.de>
2012-05-14 16:08:46 +0200

    Signed-off-by: Christian <christian.wernicke1@web.de>

--- strategie/WahrscheinlichkeitSpielzug.java
+++
@@ -1,9 +0,0 @@
-package pgm.tictactoe.strategie;
-
-import java.io.Serializable;
-
-public abstract class WahrscheinlichkeitSpielzug implements IStrategie, Serializable {	
-	protected IStrategie strategie;
-	
-	protected abstract boolean ersetzeSpielzug(int zufallsWert);
-}
