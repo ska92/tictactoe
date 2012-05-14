dec1f79da5e2f975b79af865fbc0cbc10b5250a1
Christian <christian.wernicke1@web.de>
2012-05-14 16:36:05 +0200

    Signed-off-by: Christian <christian.wernicke1@web.de>

--- Programmiermethoden/src/Game/GameController.java
+++ Programmiermethoden/src/Game/GameController.java
@@ -1,6 +1,7 @@
 package Game;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -16,7 +17,12 @@ public abstract class GameController {
 	protected Viewer viewer;
 	protected IRegeln regeln;
 	
+	private boolean startGame = true;
+	private boolean debug = false;
+	
 	public GameController(String[] args, IRegeln regeln, Model gameModel, Viewer viewer){
+		parameterAuswerten(args);
+		
 		this.regeln = regeln;
 		this.model = gameModel;
 		this.viewer = viewer;
@@ -24,7 +30,7 @@ public abstract class GameController {
 		model.addObserver(viewer);
 	}
 	
-	public void parameterAuswerten(String args[]){
+	private void parameterAuswerten(String args[]){
 		Options opts = new Options();
 		opts.addOption(new Option("h","help",false,"Ausgabe der Hilfe"));
 		opts.addOption(new Option("d","debug", false, "Debug modus setzen"));
@@ -33,14 +39,34 @@ public abstract class GameController {
 		PosixParser parser = new PosixParser();
 		try {
 			CommandLine line = parser.parse(opts, args);
+			
+			if(line.hasOption("help")){
+				HelpFormatter help = new HelpFormatter();
+				help.printHelp("Hilfe", opts);
+				startGame = false;
+			}
+			
+			if(line.hasOption("debug")){
+				debug = true;
+				
+			}
+			
+			if(line.hasOption("file")){
+				
+			}
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("Parameter fehler.");
 		}
+		
+		
 		
 	}
 	
 	public void startGame(){
+		if(!startGame)
+			return;
+		
 		System.out.printf("Neues Spiel: %s %n", this.getClass().getSimpleName());
 		System.out.printf("Spieler1: %s (%s) %n", model.getSpieler1().getName(), model.getSpieler1().getGameSymbol());
 		System.out.printf("Spieler2: %s (%s) %n", model.getSpieler2().getName(), model.getSpieler2().getGameSymbol());
